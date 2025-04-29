ad08845f782d12611a051916741c940eda6b7be9
eitanliu <eitanliu@gmail.com>
2025-04-29 08:06:27 +0800

    README.md

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -24,6 +24,7 @@ kotlin {
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
 
+            implementation(project.dependencies.platform(libs.kotlinx.coroutines.bom))
             implementation(project.dependencies.platform(libs.compose.bom))
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
